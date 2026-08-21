a7c86f90cb204f829094e02e9e64ac3759b19db3
NICHOLAS-67 <2401452D@student.tp.edu.sg>
2026-08-21 11:12:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from sklearn.base import BaseEstimator, ClassifierMixin
 import joblib
 import os
 from pathlib import Path
@@ -34,7 +35,6 @@ class ThresholdOptimizedTabPFN(BaseEstimator, ClassifierMixin):
     def predict(self, X):
         probas = self.predict_proba(X)[:, 1]
         return (probas >= self.threshold).astype(int)
-
 
 st.set_page_config(page_title="EHR AKI Risk Evaluator", layout="wide")
 st.title("🫘Predict AKI Web Application")
